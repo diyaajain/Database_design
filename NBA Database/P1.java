b17fa09c176a1ffd4797960bf07763051f6b68dd
diya <diyajain@usf.edu>
2023-01-23 12:52:50 -0500

    update

--- NBA Database/P1.java
+++ NBA Database/P1.java
@@ -39,6 +39,7 @@ public class P1 {
         else if (cmd.getCommand().equals("add_coach")) {
             String[] parameters = cmd.getParameters();
             System.out.println(parameters[0]);
+            System.out.println(parameters[9]);
             Coach c = new Coach(parameters[0], Integer.parseInt(parameters[1]), Integer.parseInt(parameters[2]), (parameters[3]),
             parameters[4], Integer.parseInt(parameters[5]), Integer.parseInt(parameters[6]), Integer.parseInt(parameters[7]), Integer.parseInt(parameters[8])
             ,parameters[9]);
